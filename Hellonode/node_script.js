c4a7148409d00bfdc489da1870f0bb31d5c5b9e9
tryrbl099 <robleit111@gmail.com>
2020-02-20 13:44:21 +0800

    Commit

--- Hellonode/node_script.js
+++
@@ -1,8 +0,0 @@
-class A{
-    getMessage(){
-        console.log("Hello node")
-
-    }
-}
-a = new A()
-a.getMessage()
---
+++ javascripting/js/revising-strings.js
@@ -0,0 +1,4 @@
+let pizza = 'pizza is alright';
+
+let pizza2 = pizza.replace('alright','wonderful');
+console.log(pizza2);
